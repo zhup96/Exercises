85a2507fe7de9b3f193550668ef752213d2b0fd7
朱朋 <zhupeng@zhupengdeMacBook-Pro.local>
2022-07-06 21:51:28 +0800

    练习提交

--- practice_1.py
+++ practice_1.py
@@ -3,8 +3,7 @@
 # Author：Zhup
 # Date ：2022/7/5 22:08
 
-# 返回给定数字前的平方和
-
+# 练习一:返回给定数字前的平方和
 
 def square_sum(number):
     result = 0
@@ -14,5 +13,15 @@ def square_sum(number):
     print(result)
 
 
+import math
+
+
+# 练习二：给定圆半径，计算圆面积
+
+def compute_area_of_circle(r):
+    return round(math.pi * r * r, 2)
+
+
 if __name__ == '__main__':
-    square_sum(number=10)
+    # square_sum(number=10)
+    print(compute_area_of_circle(r=2))
